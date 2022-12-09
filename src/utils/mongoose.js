aa8e8d633a5461826fa0f8322b71dd75d663ea94
Rog3rz <jaxortiz9@gmail.com>
2022-12-09 16:17:41 -0600

    mongooose fixed

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const uri = "mongodb://localhost:27017";
+const options = { dbName: "Task", family: 4, authSource: false };
 
-mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://localhost:27017/Task")
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((error) => console.log(error));
+  .set("strictQuery", false)
+  .connect(uri, options)
+  .then(console.log("MongoDB Connected..."))
+  .catch((err) => console.error(err));
